8956cd3c1cb877c6689a951b2b0576082c4a1fc5
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-02 15:52:24 -0800

    added borders and margin

--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -20,16 +20,16 @@ export default function Preview({
         src={`${process.env.PUBLIC_URL}/outfits/${middle}-middle.png`}
       />
       <img alt={bottom} height="150" src={`${process.env.PUBLIC_URL}/outfits/${bottom}.png`} />
+      <div id="phrase-2">
+        {phraseList.map((elem) => (
+          <p key={elem}>{elem}</p>
+        ))}
+      </div>
       <div>
         <p>You have changed the head {headCount} times</p>
         <p> You have changed the middle {middleCount} times</p>
         <p>You have changed the bottom {bottomCount} times</p>
         <p>You have added a catch phrase {phraseCount} times </p>
-        <div>
-          {phraseList.map((elem) => (
-            <p key={elem}>{elem}</p>
-          ))}
-        </div>
       </div>
     </div>
   );
